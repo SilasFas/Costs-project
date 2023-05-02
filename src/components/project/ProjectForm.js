290f23ec2c2aeaaf919f0142898635c6a991bb29
Silas <silasfas@hotmail.com>
2023-05-02 18:55:12 -0300

    página projeto

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,24 @@
+export const ProjectForm = () => {
+    return (
+        <form>
+
+            <div>
+                <input type="text" placeholder="Insira nome do projeto" />
+            </div>
+
+            <div>
+                <input type="number" placeholder="Insira o orçamento total" />
+            </div>
+
+            <div>
+                <select name="category_id" id="category_id">
+                    <option disabled /*selected*/ >Selecione a categoria</option>
+                </select>
+            </div>
+
+            <div>
+                <input type="submit" value="Criar Projeto" />
+            </div>
+        </form>
+    )
+}
